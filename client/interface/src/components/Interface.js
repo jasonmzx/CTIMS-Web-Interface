b8b064d98bbde4d82703f378417d890dbbf189d5
jasonmzx <68576119+jasonmzx@users.noreply.github.com>
2023-06-26 11:01:20 -0400

    NRRD & Menu push, using `lil-gui` for the Floating X Y Z sliders in Top-right

--- client/interface/src/components/Interface.js
+++ client/interface/src/components/Interface.js
@@ -1,44 +1,71 @@
 import React, { useRef, useEffect } from 'react';
 import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { TrackballControls } from 'three/examples/jsm/controls/TrackballControls';
 import { NRRDLoader } from 'three/examples/jsm/loaders/NRRDLoader';
+import { GUI } from 'lil-gui'; // Uncomment this if you want to use GUI
 
 const Interface = () => {
     let mount = useRef(null);
 
+    const [loadedNRRDstr , setLnrrd] = React.useState("/volume.nrrd");
+
     useEffect(() => {
         // Scene, camera, and renderer setup
         const scene = new THREE.Scene();
-        const camera = new THREE.PerspectiveCamera(75, mount.current.clientWidth / mount.current.clientHeight, 0.1, 1000);
+        const camera = new THREE.PerspectiveCamera(60, mount.current.clientWidth / mount.current.clientHeight, 0.01, 1e10);
         const renderer = new THREE.WebGLRenderer();
-            
+        
+        // Add hemisphere and directional light
+        const hemiLight = new THREE.HemisphereLight(0xffffff, 0x000000, 1);
+        scene.add(hemiLight);
+        
+        const dirLight = new THREE.DirectionalLight(0xffffff, 0.5);
+        dirLight.position.set(200, 200, 200);
+        scene.add(dirLight);
+
+        camera.position.z = 300;
+        scene.add(camera);
+
         // Mount renderer to DOM
         mount.current.appendChild(renderer.domElement);
         renderer.setSize(mount.current.clientWidth, mount.current.clientHeight);
 
-        // OrbitControls setup
-        const controls = new OrbitControls(camera, renderer.domElement);
+        // TrackballControls setup
+        const controls = new TrackballControls(camera, renderer.domElement);
+        controls.minDistance = 100;
+        controls.maxDistance = 500;
+        controls.rotateSpeed = 5.0;
+        controls.zoomSpeed = 5;
+        controls.panSpeed = 2;
         controls.update();
-
-        camera.position.z = 5;
 
         // NRRDLoader setup
         const loader = new NRRDLoader();
-        loader.load('/fool.nrrd', function (volume) {
-            console.log("Loaded...");
-            // Assuming volume contains the volume data for the NRRD file
-            var geometry = new THREE.BoxGeometry( volume.xLength, volume.yLength, volume.zLength );
-            geometry.setAttribute('position', new THREE.BufferAttribute(volume.data, 3));
-            
-            // Add material to mesh (visible from both sides)
-            var material = new THREE.MeshBasicMaterial( {color: 0x00ff00} );
-            const mesh = new THREE.Mesh(geometry, material);
+        loader.load(loadedNRRDstr, function (volume) {
+            //box helper to see the extent of the volume
+            const geometry = new THREE.BoxGeometry(volume.xLength, volume.yLength, volume.zLength);
+            const material = new THREE.MeshBasicMaterial({color: 0x00ff00});
+            const cube = new THREE.Mesh(geometry, material);
+            cube.visible = false;
+            const box = new THREE.BoxHelper(cube);
+            scene.add(box);
+            box.applyMatrix4(volume.matrix);
+            scene.add(cube);
 
-            scene.add(mesh);
+            // Add slices
+            const sliceZ = volume.extractSlice('z', Math.floor(volume.RASDimensions[2] / 4));
+            scene.add(sliceZ.mesh);
+            const sliceY = volume.extractSlice('y', Math.floor(volume.RASDimensions[1] / 2));
+            scene.add(sliceY.mesh);
+            const sliceX = volume.extractSlice('x', Math.floor(volume.RASDimensions[0] / 2));
+            scene.add(sliceX.mesh);
+
+            // Uncomment the following if you want to use GUI
+            const gui = new GUI();
+            gui.add(sliceX, 'index', 0, volume.RASDimensions[0], 1).onChange(() => sliceX.repaint());
+            gui.add(sliceY, 'index', 0, volume.RASDimensions[1], 1).onChange(() => sliceY.repaint());
+            gui.add(sliceZ, 'index', 0, volume.RASDimensions[2], 1).onChange(() => sliceZ.repaint());
         });
-
-
-        console.log("Adding??");
 
         const animate = function () {
             requestAnimationFrame(animate);
@@ -55,6 +82,7 @@ const Interface = () => {
             renderer.setSize(width, height);
             camera.aspect = width / height;
             camera.updateProjectionMatrix();
+            controls.handleResize(); // update controls on window resize
         }
         
         window.addEventListener('resize', handleResize);
@@ -64,10 +92,18 @@ const Interface = () => {
             window.removeEventListener('resize', handleResize);
             mount.current.removeChild(renderer.domElement);
         };
-    }, []);
+    }, [loadedNRRDstr]);
 
     return (
-        <div ref={mount} style={{width: '100%', height: '100vh'}} />
+        <>
+        <div ref={mount} style={{width: '100%', height: '95vh'}} />
+        <div style={{height : '4vh'}} >
+            
+             MENU <button onClick={() => {setLnrrd('/I.nrrd')}}> SET TO I. NRRD Instead </button>
+             
+             
+             </div>
+        </>
     );
 }
 
